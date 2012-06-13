c9902b3bd0bc0f8d68063d348654eb3cec404654
Oliver Schneider <mail@oli-obk.de>
2012-06-13 12:11:26 +0200

    added more slides, fixed text size

---
+++ slides/test.cpp
@@ -0,0 +1,20 @@
+
+class MyClass
+{
+private:
+  int myInt;
+public:
+  int getInt() const
+  {
+    return this->myInt;
+  }
+  void bad() const
+  {
+    this->myInt = 99;
+  }
+  void setInt(int value)
+  {
+    if(value < 0) this->myInt = 0;
+    else this->myInt = value;
+  }
+};
